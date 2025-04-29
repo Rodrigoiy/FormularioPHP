328be40f58d7ad4aa13343e2f0b1be42d8a26c18
Rodrigoiy <rodrigoissamuyakushijin@gmail.com>
2025-04-29 14:16:02 -0300

    Conexão com o banco de dados adcionada

--- formulario.php
+++ formulario.php
@@ -1,3 +1,24 @@
+<?php 
+
+    if(isset($_POST['submit']))
+    {
+
+    include_once('config.php');
+
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $telefone = $_POST['telefone'];
+    $sexo = $_POST['genero'];
+    $data_nasc = $_POST['data_nascimento'];
+    $cidade = $_POST['cidade'];
+    $estado = $_POST['estado'];
+    $endereco = $_POST['endereco'];
+
+    $result = mysqli_query($conexao, "INSERT INTO usuarios (nome, email, telefone, sexo, data_nasc, cidade, estado, endereco) 
+    VALUES ('$nome', '$email', '$telefone', '$sexo', '$data_nasc', '$cidade', '$estado', '$endereco')");
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -8,7 +29,7 @@
 </head>
 <body>
     <div class="box">
-        <form action="">
+        <form action="formulario.php" method="POST">
             <fieldset>
                 <legend><strong>Formulário de Clientes</strong></legend>
                 <br>
